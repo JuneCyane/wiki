93ea93e810eec41db442eb9b32073b03912286cc
JuneCyane <936975165@qq.com>
2021-04-19 21:29:48 +0800

    2-9 集成热部署

--- src/main/java/com/cyane/wiki/controller/TestController.java
+++ src/main/java/com/cyane/wiki/controller/TestController.java
@@ -1,14 +1,20 @@
 package com.cyane.wiki.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
     //接口对应的请求地址
     @GetMapping("/hello")
     public String hello(){
-        return "Hello world";
+        return "Hello world!"+ testHello;
     }
     @PostMapping("hello/post")
     public String helloPost(String name){
