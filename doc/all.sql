b77b8491f9777fa93626f10f795e027171c3f486
JuneCyane <936975165@qq.com>
2021-04-19 22:53:07 +0800

    3-5 集成mybatis generator，生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -1,9 +1,21 @@
 drop table if exists `test`;
-create table `test`(
-                       `id` bigint not null comment '评论',
-                       `name` varchar(50) comment '名称',
-                       `password` varchar(50) comment '密码',
-                       primary key (`id`)
-)engine =innodb default charset =utf8mb4 comment ='测试';
+create table `test`
+(
+    `id`       bigint not null comment '评论',
+    `name`     varchar(50) comment '名称',
+    `password` varchar(50) comment '密码',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='测试';
 
-insert into `test` (id, name, password) values (1, '测试', 'password');
+insert into `test` (id, name, password)
+values (1, '测试', 'password');
+
+drop table if exists `demo`;
+create table `demo`
+(
+    `id`   bigint not null comment '评论',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='测试';
