d8bc308c9f9579d87195df356b2a81e4304e643e
JuneCyane <936975165@qq.com>
2021-04-19 14:56:59 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/cyane/wiki/controller/TestController.java
+++ src/main/java/com/cyane/wiki/controller/TestController.java
@@ -2,11 +2,16 @@ package com.cyane.wiki.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 public class TestController {
     //接口对应的请求地址
     @GetMapping("/hello")
     public String hello(){
         return "Hello world";
+    }
+    @PostMapping("hello/post")
+    public String helloPost(String name){
+        return "Hello World Post"+name;
     }
 }
