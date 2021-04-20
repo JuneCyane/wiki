b96bea716f71f2e9a45088d39578e05c77dd96c1
JuneCyane <936975165@qq.com>
2021-04-20 10:33:42 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/cyane/wiki/controller/EbookController.java
+++ src/main/java/com/cyane/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.cyane.wiki.controller;
 
-import com.cyane.wiki.domain.Ebook;
+import com.cyane.wiki.req.EbookReq;
 import com.cyane.wiki.resp.CommonResp;
+import com.cyane.wiki.resp.EbookResp;
 import com.cyane.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp= new CommonResp<>();
-        List<Ebook> list= ebookService.list();
+    public CommonResp list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp= new CommonResp<>();
+        List<EbookResp> list= ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/cyane/wiki/service/EbookService.java
+++ src/main/java/com/cyane/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.cyane.wiki.service;
 
 import com.cyane.wiki.domain.Ebook;
+import com.cyane.wiki.domain.EbookExample;
 import com.cyane.wiki.mapper.EbookMapper;
+import com.cyane.wiki.req.EbookReq;
+import com.cyane.wiki.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,7 +18,21 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list() {
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req) {
+        //写法固定
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%"+req.getName()+"%");
+        List<Ebook> ebooklist = ebookMapper.selectByExample(ebookExample);
+        //从List<Ebook>变为List<EbookResp>
+        List<EbookResp> respList=new ArrayList<>();
+        for (Ebook ebook : ebooklist) {
+            //实现实体类的复制
+            EbookResp ebookResp = new EbookResp();
+            //BeanUtils.copyProperties(source,target);
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 }
