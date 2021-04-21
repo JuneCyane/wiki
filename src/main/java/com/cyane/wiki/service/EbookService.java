f60b01456737116ebb8c7c272b4bbc745d8f1054
JuneCyane <936975165@qq.com>
2021-04-21 10:43:11 +0800

    6-4 集成PageHelper插件，将mapper日志级别改为trace

--- src/main/java/com/cyane/wiki/service/EbookService.java
+++ src/main/java/com/cyane/wiki/service/EbookService.java
@@ -6,6 +6,10 @@ import com.cyane.wiki.mapper.EbookMapper;
 import com.cyane.wiki.req.EbookReq;
 import com.cyane.wiki.resp.EbookResp;
 import com.cyane.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -15,33 +19,36 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookReq req) {
-        //写法固定
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        //动态sql
-        if (!ObjectUtils.isEmpty(req.getName())){
-            criteria.andNameLike("%"+req.getName()+"%");
+        if (!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
         }
-        List<Ebook> ebooklist = ebookMapper.selectByExample(ebookExample);
-        //从List<Ebook>变为List<EbookResp>
-/*        List<EbookResp> respList=new ArrayList<>();
-        for (Ebook ebook : ebooklist) {
-            //实现实体类的复制
-*/
-        /*  EbookResp ebookResp = new EbookResp();
-            //BeanUtils.copyProperties(source,target);
-            BeanUtils.copyProperties(ebook,ebookResp);
-        */
-        /*
+        PageHelper.startPage(1, 3);
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
-            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
-            respList.add(ebookResp);
-        }*/
-        List<EbookResp> respList = CopyUtil.copyList(ebooklist, EbookResp.class);
-        return respList;
+        // List<EbookResp> respList = new ArrayList<>();
+        // for (Ebook ebook : ebookList) {
+        //     // EbookResp ebookResp = new EbookResp();
+        //     // BeanUtils.copyProperties(ebook, ebookResp);
+        //     // 对象复制
+        //     EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+        //
+        //     respList.add(ebookResp);
+        // }
+
+        // 列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
+        return list;
     }
 }
